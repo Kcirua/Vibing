b65d7891f6c7debc25f87159c954ab50cb4cd2f2
Kcirua <auricktan@gmail.com>
2026-07-16 22:01:36 +0700

    frontend

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// 1. Beritahu Express bahwa kita menggunakan EJS
 app.set('view engine', 'ejs');
-
-// 2. KUNCI UTAMA: Arahkan Express untuk mencari file EJS di 'frontend/src/pages'
 app.set('views', path.join(__dirname, '../frontend/src/pages'));
-
-// 3. Izinkan Express membaca file statis (CSS/JS Murni/Gambar) di 'frontend/public'
 app.use(express.static(path.join(__dirname, '../frontend/public')));
 
-// Rute untuk halaman utama
 app.get('/', (req, res) => {
-  // Panggil 'Index' (sesuaikan dengan huruf kapital di nama filemu)
   res.render('Index', { 
-    judul: 'Surplus Market', 
-    peserta: 'Tim Hackathon' 
+    judul: 'AgriHub Dashboard', 
+    petani: 'Pak Budi' 
+  });
+});
+
+// Route 2: Halaman Marketplace
+app.get('/marketplace', (req, res) => {
+  res.render('Marketplace', { 
+    judul: 'Marketplace Panen'
   });
 });
 
